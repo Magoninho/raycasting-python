19e292466bb8f4bb39537fa414bcac099fa820f4
Magoninho <magoninhogamer@gmail.com>
2023-07-07 18:19:19 -0300

    THE MAP

---
+++ Map.py
@@ -0,0 +1,33 @@
+import pygame
+from settings import *
+
+class Map:
+    def __init__(self):
+        self.grid = [
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 1, 1, 0, 1, 0, 0, 1],
+            [1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 1, 0, 1, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        ]
+
+    # checks if there is a wall at a certain coordinate (in pixels)
+    def hasWallAt(self, x, y):
+        return self.grid[y // TILESIZE][x // TILESIZE]
+    
+    def render(self, screen):
+        for i in range(len(self.grid)):
+            for j in range(len(self.grid[0])):
+                # pixel coordinates
+                tile_x = j * TILESIZE
+                tile_y = i * TILESIZE
+
+                if self.grid[i][j] == 0:
+                    pygame.draw.rect(screen, (255, 255, 255), (tile_x, tile_y, TILESIZE - 1, TILESIZE - 1))
+                elif self.grid[i][j] == 1:
+                    pygame.draw.rect(screen, (40, 40, 40), (tile_x, tile_y, TILESIZE - 1, TILESIZE - 1))
--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
 from settings import *
+from Map import *
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
+map = Map()
+
 while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			pygame.quit()
-			exit()
-	
-	screen.fill((0,0,0))
-	
-	pygame.display.update()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    
+    screen.fill((0,0,0))
+
+    map.render(screen)
+    
+    pygame.display.update()
